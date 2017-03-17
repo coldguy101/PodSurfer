a375682c83cdc7549c20a7b17d0b88c57cb1352a
Mitchell Shannon <mitchell_shannon@baylor.edu>
2017-03-17 17:17:38 -0500

    Added Review Endpoints

--- src/main/java/com/credera/reviews/ReviewController.java
+++ src/main/java/com/credera/reviews/ReviewController.java
@@ -1,6 +1,8 @@
 package com.credera.reviews;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,14 +10,40 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by Mitchell on 2/19/17.
  */
 @RestController
+
+@RequestMapping("/api/reviews")
 public class ReviewController {
 
     @Autowired
     private ReviewService revService;
 
-    @RequestMapping("/api/reviews")
+    @RequestMapping("/all")
     public ReviewModel[] getReviews() {
         return revService.getAllReviews();
     }
 
+
+    @RequestMapping("/create")
+    public String createNewReview(@RequestHeader HttpHeaders headers) {
+        System.out.println("Headers: " + headers);
+        return revService.createNewReview(headers);
+    }
+
+    @RequestMapping("/delete")
+    public String deleteReview(@RequestHeader HttpHeaders headers) {
+        System.out.println("Headers: " + headers);
+        return revService.deleteReview(headers);
+    }
+
+    @RequestMapping("/getMy")
+    public String getMyReviews(@RequestHeader HttpHeaders headers) {
+        System.out.println("Headers: " + headers);
+        return revService.getMyReviews(headers);
+    }
+
+    @RequestMapping("/update")
+    public String updateReview(@RequestHeader HttpHeaders headers) {
+        System.out.println("Headers: " + headers);
+        return revService.updateReview(headers);
+    }
 }
--- src/main/java/com/credera/reviews/ReviewService.java
+++ src/main/java/com/credera/reviews/ReviewService.java
@@ -1,5 +1,9 @@
 package com.credera.reviews;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,5 +22,45 @@ public class ReviewService {
         for(ReviewModel pmm : pm)
             System.out.println(pmm);
         return pm;
+    }
+
+    public String createNewReview(HttpHeaders headers) {
+        RestTemplate rt = new RestTemplate();
+
+        HttpEntity<HttpHeaders> head = new HttpEntity<>(headers);
+
+        ResponseEntity<String> res = rt.exchange("https://podsurfer-spring3.herokuapp.com/api/review/", HttpMethod.POST, head, String.class);
+
+        return res.getBody();
+    }
+
+    public String deleteReview(HttpHeaders headers) {
+        RestTemplate rt = new RestTemplate();
+
+        HttpEntity<HttpHeaders> head = new HttpEntity<>(headers);
+
+        ResponseEntity<String> res = rt.exchange("https://podsurfer-spring3.herokuapp.com/api/review/:id", HttpMethod.DELETE, head, String.class);
+
+        return res.getBody();
+    }
+
+    public String getMyReviews(HttpHeaders headers) {
+        RestTemplate rt = new RestTemplate();
+
+        HttpEntity<HttpHeaders> head = new HttpEntity<>(headers);
+
+        ResponseEntity<String> res = rt.exchange("https://podsurfer-spring3.herokuapp.com/api/review/mine", HttpMethod.GET, head, String.class);
+
+        return res.getBody();
+    }
+
+    public String updateReview(HttpHeaders headers) {
+        RestTemplate rt = new RestTemplate();
+
+        HttpEntity<HttpHeaders> head = new HttpEntity<>(headers);
+
+        ResponseEntity<String> res = rt.exchange("https://podsurfer-spring3.herokuapp.com/api/review/:id", HttpMethod.PUT, head, String.class);
+
+        return res.getBody();
     }
 }
