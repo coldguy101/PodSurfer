35a7c0b7cae193dfa8cf4790926172e0f5a184c9
LAPTOP-6FA0MHJL\Nathan <nathan_eden@baylor.edu>
2017-03-21 20:27:02 -0500

    Added testing layer

--- src/test/java/com/credera/reviews/ReviewControllerTest.java
+++ src/test/java/com/credera/reviews/ReviewControllerTest.java
@@ -1,8 +1,36 @@
+package com.credera.reviews;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by Nathan on 3/21/2017.
  */
 public class ReviewControllerTest {
+    @Test
+    public void getReviews() throws Exception {
+
+    }
+
+    @Test
+    public void createNewReview() throws Exception {
+
+    }
+
+    @Test
+    public void deleteReview() throws Exception {
+
+    }
+
+    @Test
+    public void getMyReviews() throws Exception {
+
+    }
+
+    @Test
+    public void updateReview() throws Exception {
+
+    }
 
 }
--- src/test/java/com/credera/users/UserControllerTest.java
+++ src/test/java/com/credera/users/UserControllerTest.java
@@ -1,8 +1,53 @@
+package com.credera.users;
+
+import com.sun.security.auth.callback.DialogCallbackHandler;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by Nathan on 3/21/2017.
  */
-public class UserControllerTest {
+@RunWith(SpringJUnit4ClassRunner.class)
 
+public class UserControllerTest {
+    StandaloneMockMvcBuilder mockMvc;
+
+    @Before
+    public void setup() {
+
+        mockMvc = MockMvcBuilders.standaloneSetup();
+
+    }
+
+    @Test
+    public void login() throws Exception {
+    UserController.class.newInstance().login()
+    }
+
+    @Test
+    public void getYourInfo() throws Exception {
+
+    }
+
+    @Test
+    public void create() throws Exception {
+
+    }
+    @Test
+    public void testHome() throws Exception {
+
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(forwardedUrl("WEB-INF/pages/index.jsp"));
+
+    }
 }
