4f4e85d92b2f3d62d726b67c95fd2188dafcc1e6
Sean Lavoie <sean@n2c7.com>
2017-04-12 00:41:10 -0500

    Fixed everything.

--- src/main/resources/public/app/podcast/podcasts.component.ts
+++ src/main/resources/public/app/podcast/podcasts.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { PodcastService } from './podcast.service';
+import { LoginService } from "../login/login.service";
+import { ProfileService } from "../profile/profile.service";
 
 @Component ({
   selector: 'podcast',
   templateUrl: './app/podcast/podcasts.html',
-  providers: [ PodcastService ]
+  providers: [ PodcastService, LoginService, ProfileService ]
 })
 export class PodcastsComponent implements OnInit{
   podcasts: any;
+  loggedIn: boolean = false;
   selection: string;
 
-  constructor(private podcastService: PodcastService) {}
+  constructor(private podcastService: PodcastService,
+              private loginService: LoginService,
+              private profileService: ProfileService) {}
 
   ngOnInit() {
     const that = this;
@@ -20,10 +25,26 @@ export class PodcastsComponent implements OnInit{
     };
 
     this.podcastService.getAllPodcasts().then(success);
+    this.loggedIn = this.loginService.isLoggedIn();
+    if(this.loggedIn)
+      this.profileService.getBookmarks(this.loginService.getToken()); //Must call once :/
+          //This is made incorrectly and will fail if the podcasts load before the callback finishes for this.
   }
 
   rmPodcast(pcast: any) {
     delete this.podcasts[this.podcasts.indexOf(pcast)];
     this.podcastService.rmPodcast(pcast._id);
+  }
+
+  favoritePodcast(podID: string) {
+    this.profileService.addBookmark(podID, this.loginService.getToken());
+  }
+
+  unFavoritePodcast(podID: string) {
+    this.profileService.removeBookmark(podID, this.loginService.getToken());
+  }
+
+  isPodcastFavorited(podID: string): boolean {
+    return this.profileService.isPodcastBookmarked(podID, this.loginService.getToken());
   }
 }
--- src/main/resources/public/app/profile/profile.service.ts
+++ src/main/resources/public/app/profile/profile.service.ts
@@ -5,6 +5,8 @@ import 'rxjs/add/operator/toPromise';
 @Injectable()
 export class ProfileService {
 
+  private bookmarks: string[];
+
   constructor(private http: Http) {}
 
   getProfile(token: string): Promise<any> {
@@ -27,14 +29,50 @@ export class ProfileService {
       .catch(this.handleError);
   }
 
-  setProfile(profile: any, token: string) {
+  updateProfile(profile: any, token: string) {
     console.log(profile);
     let headers: Headers = new Headers();
-    //headers.append('Authorization', 'Bearer ' + token);
-    //return this.http.put("/api/user/update", { headers: headers })
-    //  .subscribe(res => {
-    //    console.log(res);
-    //  });
+    headers.append('Authorization', 'Bearer ' + token);
+    return this.http.put("/api/user/update", profile, { headers: headers })
+     .subscribe(res => {
+       console.log(res);
+     });
+  }
+
+  getBookmarks(token: string): string[] {
+    let that = this;
+
+    let success = function(values: any) {
+      that.bookmarks = values.bookmarks;
+    };
+
+    this.getProfile(token).then(success);
+
+    return this.bookmarks;
+  }
+
+  getBookmarksPromise(token: string) {
+    return this.getProfile(token).then(res => res.bookmarks)
+  }
+
+  addBookmark(id: string, token: string) {
+    this.bookmarks.push(id);
+    this.updateProfile({bookmarks: this.bookmarks}, token);
+  }
+
+  removeBookmark(id: string, token: string) {
+    let index = this.bookmarks.indexOf(id);
+    if(index !== -1) {
+      if (index > -1) {
+        this.bookmarks.splice(index, 1);
+      }
+    }
+    //delete this.bookmarks[index];
+    this.updateProfile({bookmarks: this.bookmarks}, token);
+  }
+
+  isPodcastBookmarked(id: string, token: string) {
+    return this.bookmarks.indexOf(id) !== -1;
   }
 
   private handleError(error: any): PromiseLike<any> {
